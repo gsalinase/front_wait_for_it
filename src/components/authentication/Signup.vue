08889b774ccce8508125303090cb7a93b46ed4ae
Gonzalo Salinas <gsalinase91@gmail.com>
2020-09-02 22:33:28 -0400

    Add home

--- src/components/authentication/Signup.vue
+++ src/components/authentication/Signup.vue
@@ -1,55 +1,66 @@
 <template>
   <div>
-    <div class="signup--wrapper">
-      <h1 class="title">Crean una cuenta en W4IT</h1>
-      <div class="columns">
+    <div class="columns is-gapless is-centered h-100">
+      <form @submit.prevent="register">
         <div class="column">
-          <b-field label="Nombre Completo">
-            <b-input v-model="name" placeholder="John Doe"></b-input>
-          </b-field>
-        </div>
-      </div>
-      <div class="columns">
-        <div class="column">
-          <b-field label="Número de Teléfono">
-            <b-input v-model="phone" placeholder="9 1234 5678" type="number" max="9"></b-input>
-          </b-field>
-        </div>
-        <div class="column">
-          <b-field label="¿Cuándo es tu compleaños?">
-            <b-datepicker v-model="birthday" placeholder="Selecciona tu fecha de cumpleaños" icon="calendar-today"
-              trap-focus>
-            </b-datepicker>
-          </b-field>
-        </div>
-      </div>
-      <div class="columns">
-        <div class="column">
-          <label class="label">Correo Electrónico</label>
-          <b-field>
-            <b-input placeholder="Ingrese su correo" v-model="email" type="email" icon="email" icon-right="close-circle"
-              icon-right-clickable>
-            </b-input>
-          </b-field>
-        </div>
-        <div class="column">
-          <label class="label">Contraseña</label>
-          <b-field>
-            <b-input v-model="password" type="password" placeholder="Escriba su contraseña" password-reveal>
-            </b-input>
-          </b-field>
-        </div>
-      </div>
+          <div class="px-6 py-6">
+            <div class="pb-5">
+              <img width="50" src="../../assets/logo.svg" alt="Logo Wait for It">
+            </div>
+            <div class="signup--wrapper">
+              <h1 class="title">Crean una cuenta en W4IT</h1>
+              <div class="columns">
+                <div class="column">
+                  <b-field label="Nombre Completo">
+                    <b-input v-model="name" placeholder="John Doe"></b-input>
+                  </b-field>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <b-field label="Número de Teléfono">
+                    <b-input v-model="phone" placeholder="9 1234 5678" type="number"></b-input>
+                  </b-field>
+                </div>
+                <div class="column">
+                  <b-field label="¿Cuándo es tu compleaños?">
+                    <b-datepicker v-model="birthday" placeholder="Selecciona tu fecha de cumpleaños"
+                      icon="calendar-today" trap-focus>
+                    </b-datepicker>
+                  </b-field>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <label class="label">Correo Electrónico</label>
+                  <b-field>
+                    <b-input placeholder="Ingrese su correo" v-model="email" type="email" icon="email"
+                      icon-right="close-circle" icon-right-clickable>
+                    </b-input>
+                  </b-field>
+                </div>
+                <div class="column">
+                  <label class="label">Contraseña</label>
+                  <b-field>
+                    <b-input v-model="password" type="password" placeholder="Escriba su contraseña" password-reveal>
+                    </b-input>
+                  </b-field>
+                </div>
+              </div>
 
-      <div class="field py-3">
-        <b-checkbox v-model="checkbox" true-value="Sí" false-value="No">
-          ¿Tu trabajo es de primera necesidad?
-        </b-checkbox>
-      </div>
-    </div>
-
-    <div class="buttons py-5">
-      <b-button type="is-primary" @click="sendSignup">Crear Cuenta</b-button>
+              <div class="field py-3">
+                <b-checkbox v-model="checkbox" true-value="Sí" false-value="No">
+                  ¿Tu trabajo es de primera necesidad?
+                </b-checkbox>
+              </div>
+            </div>
+            <div class="buttons py-5">
+              <b-button type="is-primary" native-type="submit" expanded>Crear Cuenta</b-button>
+            </div>
+          </div>
+        </div>
+      </form>
+      <div class="column signup--bg"></div>
     </div>
   </div>
 </template>
@@ -91,4 +102,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .h-100 {
+    height: 100vh;
+  }
+
+  .signup--bg {
+    background-image: url('../../assets/signup.jpg');
+  }
+
 </style>
